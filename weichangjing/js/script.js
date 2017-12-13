891021bdbdaf90a29798d7e00dc9a280e6577f7e
yangwenbo2 <34323660+yangwenbo2@users.noreply.github.com>
2017-12-13 15:20:46 +0800

    微场景动画版

--- weichangjing/js/script.js
+++ weichangjing/js/script.js
@@ -1,3 +1,4 @@
+
 var swiper = new Swiper('.swiper-container', {
       direction: 'vertical',
       pagination: {
@@ -7,9 +8,27 @@ var swiper = new Swiper('.swiper-container', {
      // 如果需要前进后退按钮
     navigation: {
       nextEl: '.swiper-button-next',
-      
     },
+    on:{
+      init: function(){
+        swiperAnimateCache(this); //隐藏动画元素 
+        swiperAnimate(this); //初始化完成开始动画
+      }, 
+      slideChangeTransitionEnd: function(){ 
+        swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+      } 
+    }
     });
 var mySwiper = new Swiper('.swiper-container',{
   grabCursor : true,
 })
+
+var mp3 = document.querySelector("#mp3");
+var mp3btn = document.querySelector(".mp3btn");
+mp3btn.onclick = function(){
+    if(mp3.paused){
+        mp3.play();
+    }else{
+        mp3.pause();
+    }
+}
